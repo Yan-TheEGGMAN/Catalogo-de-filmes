b8f278e7d10603209eb290686947c11604eb40be
Yan-TheEGGMAN <discorddoyan1998@gmail.com>
2026-04-09 17:00:57 -0300

    v0.2.1

--- back-end/criarNovo.js
+++ back-end/criarNovo.js
@@ -1,7 +1,7 @@
 function criarElemento(lista){  //Que deus me ajude
     let catalogo = document.getElementById("catalogo")
 
-    lista.forEach(item => {
+    lista.forEach((item, index)=> {
         let card = document.createElement("div")
         let img = document.createElement("img")
         let nome = document.createElement("h2")
@@ -14,6 +14,7 @@ function criarElemento(lista){  //Que deus me ajude
         card.appendChild(img)
         card.appendChild(nome)
         card.appendChild(descricao)
+        card.id = `produto-${index}`
 
         catalogo.appendChild(card)
     })
@@ -24,6 +25,7 @@ let produtos=[
     {
         imagem: "https://img.elo7.com.br/product/zoom/268BA70/big-poster-sonic-o-filme-lo01-tamanho-90x60-cm-poster-cinema.jpg",
         nome:"Sonic The Fucking movie",
-        descricao:"O FILME INCRIVEL DO SONIC IRADISSIMO"
+        descricao:"O FILME INCRIVEL DO SONIC IRADISSIMO",
+        id: 01
     }
 ]
