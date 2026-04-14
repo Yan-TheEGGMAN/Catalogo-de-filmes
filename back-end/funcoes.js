cbde086ec722c7510b9ad1f68eefe4160ba2c8fb
Yan-TheEGGMAN <discorddoyan1998@gmail.com>
2026-04-14 14:32:29 -0300

    v0.4

--- back-end/funcoes.js
+++ back-end/funcoes.js
@@ -1,28 +1,5 @@
-function criarElemento(lista){  //Que deus me ajude
-    let catalogo = document.getElementById("catalogo")
-
-    lista.forEach((item, index)=> {
-        let card = document.createElement("div")
-        let img = document.createElement("img")
-        let nome = document.createElement("h2")
-        let descricao = document.createElement("p")
-
-        img.src = item.imagem
-        nome.innerText = item.nome
-        descricao.innerText = item.descricao
-
-        card.appendChild(img)
-        card.appendChild(nome)
-        card.appendChild(descricao)
-        card.id = `produto-${index}`
-
-        catalogo.appendChild(card)
-    })
-
-}
-
 function carregar(){
-    fetch("dados.js").then(response => response.json()) //Pega os dados do JSON e converte para o formato
+    fetch("../back-end/produtos.json").then(response => response.json()) //Pega os dados do JSON e converte para o formato
     .then(produtos => {
         const container =  document.getElementById("catalogo")
         
