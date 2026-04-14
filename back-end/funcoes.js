81de81be258f68642de735a6c2e050f5c4018952
Yan-TheEGGMAN <discorddoyan1998@gmail.com>
2026-04-14 15:19:44 -0300

    v0.4.1

--- back-end/funcoes.js
+++ back-end/funcoes.js
@@ -28,3 +28,18 @@ function carregar(){
         })
     })
 }
+
+function adicionar(){
+    const form = document.querySelector(".form")
+
+    form.addEventListener("submit", event => {
+
+        event.preventDefault()
+
+        const formData = new FormData(form)
+        const data = Object.fromEntries(formData)
+
+        
+
+    })
+}
